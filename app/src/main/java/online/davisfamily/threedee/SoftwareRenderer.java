46d8f5d93bedf69e0d78dda6a691890a6137a98b
Richard Davis <rich@davisfamily.online>
2026-01-29 14:17:41 +0000

    Added triangle scanline fill

--- app/src/main/java/online/davisfamily/threedee/SoftwareRenderer.java
+++ app/src/main/java/online/davisfamily/threedee/SoftwareRenderer.java
@@ -11,8 +11,8 @@ import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
-import online.davisfamily.threedee.cohensutherland.CohenSutherlandLineClip;
-import online.davisfamily.threedee.cohensutherland.CohenSutherlandLineClip.CohenSutherlandLineClipResult;
+import online.davisfamily.threedee.cohensutherland.CohenSutherlandLineClipper;
+import online.davisfamily.threedee.cohensutherland.CohenSutherlandLineClipper.LineClipResults;
 
 
 public class SoftwareRenderer extends JPanel {
@@ -26,10 +26,12 @@ public class SoftwareRenderer extends JPanel {
 	private int vpMaxXExclusive;
 	private int vpMaxYExclusive;
 	
+
 	private final BufferedImage image;
 	private final int[] pixels; //pixel argb values - multiply y coord by width and add x coord for pixel argb value
 	
-	private CohenSutherlandLineClip clipper;
+	private CohenSutherlandLineClipper clipper;
+	private LineClipResults lcResults;
 	
 	public SoftwareRenderer (int width, int height, int minX, int minY, int maxX, int maxY) {
 		this.width = width;
@@ -42,7 +44,8 @@ public class SoftwareRenderer extends JPanel {
 		
 		this.image = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_ARGB);
 		this.pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
-		this.clipper = new CohenSutherlandLineClip(vpMinX, vpMinY, vpMaxXExclusive-1, vpMaxYExclusive-1);		
+		this.clipper = new CohenSutherlandLineClipper(vpMinX, vpMinY, vpMaxXExclusive-1, vpMaxYExclusive-1);
+		this.lcResults = new LineClipResults();
 		setPreferredSize(new Dimension(width, height));
 	}
 	
@@ -61,16 +64,6 @@ public class SoftwareRenderer extends JPanel {
 		pixels[y*width + x] = argb;
 	}
 */	
-	private void renderFrame(double tSeconds) {
-		int colour = 0xFFFFCC00;
-		clear(0xFF101018);
-		
-		int dY = vpMaxYExclusive - vpMinY;
-		int dX = vpMaxXExclusive - vpMinX;
-		 
-		// triangle with no clip
-		drawTriangle (vpMinX, vpMinY, vpMinX, vpMinY + (dY / 2), vpMinX + (dX / 2), vpMinY + (dY / 2), colour);
-	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -127,35 +120,192 @@ public class SoftwareRenderer extends JPanel {
 	
 	public void drawLineBresenhamUnsafeClipped(int x0, int y0, int x1, int y1, int colour) {
 		
-		CohenSutherlandLineClipResult viewportLine = clipper.computeViewportLine(x0, y0, x1, y1);
-		if (!viewportLine.accept) return;
+		boolean accepted = clipper.computeViewportLine(x0, y0, x1, y1, lcResults);
+		if (!accepted) return;
 
-		int deltaX = Math.abs(viewportLine.x1 - viewportLine.x0);
-		int deltaY = Math.abs(viewportLine.y1 - viewportLine.y0);
+		int deltaX = Math.abs(lcResults.x1 - lcResults.x0);
+		int deltaY = Math.abs(lcResults.y1 - lcResults.y0);
 		
 		if (deltaY < deltaX) {
-			if (viewportLine.x0 > viewportLine.x1)
-				lineLow(viewportLine.x1, viewportLine.y1, viewportLine.x0, viewportLine.y0, colour);
+			if (lcResults.x0 > lcResults.x1)
+				lineLow(lcResults.x1, lcResults.y1, lcResults.x0, lcResults.y0, colour);
 			else
-				lineLow(viewportLine.x0, viewportLine.y0, viewportLine.x1, viewportLine.y1, colour);
+				lineLow(lcResults.x0, lcResults.y0, lcResults.x1, lcResults.y1, colour);
 		} else {
-			if (viewportLine.y0 > viewportLine.y1)
-				lineHigh(viewportLine.x1, viewportLine.y1, viewportLine.x0, viewportLine.y0, colour);
+			if (lcResults.y0 > lcResults.y1)
+				lineHigh(lcResults.x1, lcResults.y1, lcResults.x0, lcResults.y0, colour);
 			else
-				lineHigh(viewportLine.x0, viewportLine.y0, viewportLine.x1, viewportLine.y1, colour);
+				lineHigh(lcResults.x0, lcResults.y0, lcResults.x1, lcResults.y1, colour);
 		}
 	}
 
 	private void drawTriangle (int x0, int y0, int x1, int y1, int x2, int y2, int colour) {
+		
 		drawLineBresenhamUnsafeClipped(x0, y0, x1, y1, colour);
 		drawLineBresenhamUnsafeClipped(x1, y1, x2, y2, colour);
 		drawLineBresenhamUnsafeClipped(x2, y2, x0, y0, colour);
 	}
 	
+	private String verticesAsString (int []v) {
+		StringBuffer b = new StringBuffer();
+		for (int i = 0; i < v.length; i+=2) {
+			b.append(String.format("(%d, %d),", v[i], v[i+1]));
+		}
+		b.setLength(b.length()-1);
+		return b.toString();
+	}
+
+	// unsafe sort - make sure [] length is % 2 and >= 4
+	private int[] unsafeSortPolygonVerticesByY (int[] v) {		
+		boolean stillSwapping = true;
+		while (stillSwapping) {
+			stillSwapping = false;
+			for (int c = 1; c <= v.length - 3; c+=2) {
+				if (v[c] > v[c+2]) {
+					int tx = v[c+1];
+					int ty = v[c+2];
+					v [c+1] = v[c-1];
+					v [c+2] = v[c];
+					v[c-1]=tx;
+					v[c]=ty;
+					stillSwapping = true;
+				}
+			}
+		}
+		return v;
+	}
+	
+	private void fillTriangle(int x0, int y0, int x1, int y1, int x2, int y2, int colour) {
+		// Flat vertices array, triangle is formed from points A,B,C
+		// A = v[0],v[1]; B=v[2],v[3];c=v[4],v[5]
+		// Edges are A->B, A->C, B->C
+		// AB = v[0],v[1],v[2],v[3]
+		// AC = v[0],v[1],v[4],v[5]
+		// BC = v[2],v[3], v[4],v[5]
+		int v[] = new int[] {x0, y0, x1, y1, x2, y2};
+		// sort the vertices in Y order ascending
+		v = unsafeSortPolygonVerticesByY(v);
+
+		// description defines sorted edges as AB, AC, BC
+		// do top half of triangle, so edges AB & AC 
+		// AB is v[0],v[1] to v[2],v[3] & AC is v[0],v[1]   
+		// find the x increment for each edge on the scanline
+		int abdx = v[2] - v[0]; // delta x
+		int abdy = v[3] - v[1]; // delta y
+		float abX = (float)abdx / (float)abdy;
+
+		int acdx = v[4] - v[0];
+		int acdy = v[5] - v[1];
+		float acX = (float)acdx / (float)acdy;
+
+		// edge1xIntersection = number of pixels left edge moves as we move down y
+		// edge2xIntersection = number of pixels until intersection from left as we move down y
+		// both edges have the same intersection to start with
+		float edge1xIntersection = (float)v[0];
+		float edge2xIntersection = (float)v[0];		
+
+
+/*
+		// for each scanline x intersection fill in the pixels between
+		for (int y=v[1]; y<v[3];y++) {
+			// workout left and right intersections for line
+			int leftx = Math.round(edge1xIntersection);
+			int rightx = Math.round(edge2xIntersection);
+
+			// ensure we always go left to right
+			if(rightx < leftx) {
+				int t=leftx;
+				leftx = rightx;
+				rightx = t;
+			}
+
+			for (int x = leftx; x<=rightx; x++)
+				pixels[y*width+x] = colour;
+
+			edge1xIntersection += abX;
+			edge2xIntersection += acX;
+		}
+*/		
+		int bcdx = v[4] - v[2];
+		int bcdy = v[5] - v[3];
+		float bcX = (float)bcdx / (float)bcdy;
+		
+		// left tracker is the AB y delta multiplied by the x increment of AC added to Ax 
+		// one of the trackers is actually in the correct position so could calculate or
+		// could do leftTracker = rightTracker < leftTracker ? rightTracker : leftTracker
+		edge1xIntersection = abdy * acX + v[0];
+//		edge2xIntersection = v[2];
+		fillHalfTriangle(v[0],v[1], acX, v[2], v[3], abX, colour, true);
+		fillHalfTriangle(Math.round(edge1xIntersection),v[3], acX, v[2], v[5], bcX, colour, false);
+/*
+		// for each scanline x intersection fill in the pixels between
+		for (int y=v[3]; y<v[5];y++) {
+			// workout left and right intersections for line
+			int leftx = Math.round(edge1xIntersection);
+			int rightx = Math.round(edge2xIntersection);
+
+			// ensure we always go left to right
+			if(rightx < leftx) {
+				int t=leftx;
+				leftx = rightx;
+				rightx = t;
+			}
+
+			for (int x = leftx; x<=rightx; x++)
+				pixels[y*width+x] = colour;
+
+			edge1xIntersection += acX;
+			edge2xIntersection += bcX;
+		}
+*/
+	}
+	
+	private void fillHalfTriangle(int e1x, int e1y, float e1xInc, int e2x, int e2y, float e2xInc, int colour, boolean isTop) {
+		// for each scanline x intersection fill in the pixels between
+		int leftx = e1x;
+		int rightx = isTop ? e1x: e2x;
+
+		float edge1xIntersection = (float) leftx;
+		float edge2xIntersection = (float) rightx;
+
+		for (int y=e1y; y<e2y;y++) {
+			// workout left and right intersections for line
+
+			// ensure we always go left to right
+			if(rightx < leftx) {
+				int t=leftx;
+				leftx = rightx;
+				rightx = t;
+			}
+
+			for (int x = leftx; x<=rightx; x++)
+				pixels[y*width+x] = colour;
+
+			edge1xIntersection += e1xInc;
+			edge2xIntersection += e2xInc;
+			leftx = Math.round(edge1xIntersection);
+			rightx = Math.round(edge2xIntersection);
+		}		
+	}
+		
+	private void renderFrame(double tSeconds) {
+		int colour = 0xFFFFCC00;
+		clear(0xFF101018);
+		
+		int dY = vpMaxYExclusive - vpMinY;
+		int dX = vpMaxXExclusive - vpMinX;
+		 
+		// triangle with no clip
+		//drawTriangle (vpMinX, vpMinY, vpMinX, vpMinY + (dY / 2), vpMinX + (dX / 2), vpMinY + (dY / 2), colour);
+		//fillTriangle (vpMinX, vpMinY, vpMinX, vpMinY + (dY / 2), vpMinX + (dX / 2), vpMinY + (dY / 2), colour);
+		fillTriangle (20, 20, 100, 400, 300, 200, colour);
+	}
+	
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(() -> {
 			int w = 960, h = 540;
-			int minX = 80, minY = 80, maxX = 120, maxY = 120; 
+			//int minX = 80, minY = 80, maxX = 120, maxY = 120;
+			int minX = 0, minY = 0, maxX = w, maxY = h;
 			SoftwareRenderer panel = new SoftwareRenderer(w,h, minX, minY, maxX, maxY);
 			JFrame frame = new JFrame("Software Renderer");
 			frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
--- app/src/main/java/online/davisfamily/threedee/cohensutherland/CohenSutherlandLineClipper.java
+++ app/src/main/java/online/davisfamily/threedee/cohensutherland/CohenSutherlandLineClipper.java
@@ -1,6 +1,6 @@
 package online.davisfamily.threedee.cohensutherland;
 
-public class CohenSutherlandLineClip {
+public class CohenSutherlandLineClipper {
 	
 	static final int INSIDE = 0;
 	static final int LEFT   = 1;
@@ -13,22 +13,15 @@ public class CohenSutherlandLineClip {
 	private int maxX;
 	private int maxY;
 
-	public CohenSutherlandLineClip(int minX, int minY, int maxX, int maxY) {
+	public CohenSutherlandLineClipper(int minX, int minY, int maxX, int maxY) {
 		  this.minX = minX; this.minY = minY; this.maxX = maxX; this.maxY = maxY;
 	}
 	
-	public static class CohenSutherlandLineClipResult {
+	public static class LineClipResults {
 		public int x0, y0, x1, y1;
-		public boolean accept;
 		
-		public CohenSutherlandLineClipResult (int x0, int y0, int x1, int y1) {
-			this.x0 = x0;this.y0=y0;this.x1=x1;this.y1=y1;accept=false;
+		public LineClipResults() {
 		}
-/*		
-		public String linePointsAsString() {
-			return "(" + x0 + ", " + y0 + ")(" + x1 + ", " + y1 + ")";
-		}
-*/
 	}
 	
 	private int computeOutcode(int x, int y) {
@@ -57,56 +50,53 @@ public class CohenSutherlandLineClip {
 		return (v < lo) ? lo : (v > hi) ? hi : v;
 	}
 	
-	public CohenSutherlandLineClipResult computeViewportLine (int x0, int y0, int x1, int y1) {
+	public boolean computeViewportLine(int x0, int y0, int x1, int y1, LineClipResults results) {
+		results.x0 = x0;results.y0 = y0;results.x1=x1;results.y1=y1;
 		int p0Outcode = computeOutcode(x0, y0);
 		int p1Outcode = computeOutcode(x1, y1);
-		CohenSutherlandLineClipResult retVal = new CohenSutherlandLineClipResult(x0, y0, x1, y1);
 
 		while (true) {
 			if ((p0Outcode | p1Outcode) == 0) {
 				// both points are inside the viewport
 				// algorithm guarantees that at some point this will happen
 				// if any part of the line is inside the viewport
-				retVal.accept = true;
-				break;
+				return true;
 			} else if ((p0Outcode & p1Outcode) != 0) {
 				// both points are outside the viewport
-				retVal.accept = false; // not strictly necessary
-				break;
+				return false; // not strictly necessary
 			} else {
 				// some part of the line is within the viewport so clip it from the point of entry
 				double x = 0, y = 0;
-				double dx = retVal.x1 - retVal.x0;
-				double dy = retVal.y1 - retVal.y0;
+				double dx = x1 - x0;
+				double dy = y1 - y0;
 				int outcodeOut = p0Outcode != 0 ? p0Outcode : p1Outcode;
 				
 				// find the intersection point of line and viewport
 				if ((outcodeOut & TOP) != 0) {
-					x = (dy == 0) ? retVal.x0 : retVal.x0 + dx * (minY - retVal.y0) / dy;
+					x = (dy == 0) ? x0 : x0 + dx * (minY - y0) / dy;
 					y = minY;
 				} else if ((outcodeOut & BOTTOM) != 0) {
-					x = (dy == 0) ? retVal.x0 : retVal.x0 + dx * (maxY - retVal.y0) / dy;
+					x = (dy == 0) ? x0 : x0 + dx * (maxY - y0) / dy;
 					y = maxY;
 				} else if ((outcodeOut & LEFT) != 0) {
-					y = (dx == 0) ? retVal.y0 : retVal.y0 + dy * (minX - retVal.x0) / dx;
+					y = (dx == 0) ? y0 : y0 + dy * (minX - x0) / dx;
 					x = minX;
 				} else {
 					// RIGHT
-					y = (dx == 0) ? retVal.y0 : retVal.y0 + dy * (maxX - retVal.x0) / dx;
+					y = (dx == 0) ? y0 : y0 + dy * (maxX - x0) / dx;
 					x = maxX;
 				}
 				
 				if (outcodeOut == p0Outcode) {
-					retVal.x0 = clamp((int) Math.round(x), minX, maxX);
-					retVal.y0 = clamp((int) Math.round(y), minY, maxY);
-					p0Outcode = computeOutcode(retVal.x0, retVal.y0); 
+					x0 = clamp((int) Math.round(x), minX, maxX);
+					y0 = clamp((int) Math.round(y), minY, maxY);
+					p0Outcode = computeOutcode(x0, y0); 
 				} else {
-					retVal.x1 = clamp((int) Math.round(x), minX, maxX);
-					retVal.y1 = clamp((int) Math.round(y), minY, maxY);
-					p1Outcode = computeOutcode(retVal.x1, retVal.y1);
+					x1 = clamp((int) Math.round(x), minX, maxX);
+					y1 = clamp((int) Math.round(y), minY, maxY);
+					p1Outcode = computeOutcode(x1, y1);
 				}
 			}
 		}
-		return retVal;
 	}
 }
