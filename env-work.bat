9b32da31340411769403b6a74391c68e0ba072c8
Richard Davis <rich@davisfamily.online>
2026-04-13 07:45:51 +0100

    Changes to env setup and requirements files.

--- env-work.bat
+++ env-work.bat
@@ -1,5 +1,6 @@
 @echo off
-set "JAVA_HOME=C:\Java\jdk\21.0.2"
+set "JAVA_HOME=C:\Java\jdk\21.0.7"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 echo JAVA_HOME set to %JAVA_HOME%
-
+set HTTP_PROXY=http://GB3WSA-Intvip.corp.internal:9480
+set HTTPS_PROXY=http://GB3WSA-Intvip.corp.internal:9480
