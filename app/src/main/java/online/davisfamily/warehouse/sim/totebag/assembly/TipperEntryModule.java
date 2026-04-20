5938b7431f20bec3904b82de692ac4967611ff43
Richard Davis <rich@davisfamily.online>
2026-04-20 13:58:52 +0100

    Extract tipper assembly render factory

--- app/src/main/java/online/davisfamily/warehouse/sim/totebag/assembly/TipperEntryModule.java
+++ app/src/main/java/online/davisfamily/warehouse/sim/totebag/assembly/TipperEntryModule.java
@@ -1,17 +1,12 @@
 package online.davisfamily.warehouse.sim.totebag.assembly;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
 import online.davisfamily.threedee.behaviour.routing.RouteFollower;
 import online.davisfamily.threedee.behaviour.routing.RouteSegment;
 import online.davisfamily.threedee.debug.SelectionInspectionRegistry;
-import online.davisfamily.threedee.matrices.Mat4;
-import online.davisfamily.threedee.matrices.Mat4.ObjectTransformation;
 import online.davisfamily.threedee.matrices.Vec3;
-import online.davisfamily.threedee.matrices.Vec4;
-import online.davisfamily.threedee.model.Mesh;
 import online.davisfamily.threedee.path.LinearSegment3;
 import online.davisfamily.threedee.rendering.RenderableObject;
 import online.davisfamily.threedee.rendering.TriangleRenderer;
@@ -21,12 +16,9 @@ import online.davisfamily.warehouse.rendering.model.tote.RenderableToteFactory;
 import online.davisfamily.warehouse.rendering.model.tote.ToteEnvelope;
 import online.davisfamily.warehouse.rendering.model.tote.ToteGeometry;
 import online.davisfamily.warehouse.rendering.model.tracks.ConveyorRuntimeState;
-import online.davisfamily.warehouse.rendering.model.tracks.RenderableTrackFactory;
-import online.davisfamily.warehouse.rendering.model.tracks.RollerMeshFactory;
 import online.davisfamily.warehouse.rendering.model.tracks.RouteTrackFactory;
 import online.davisfamily.warehouse.rendering.model.tracks.TrackAppearance;
 import online.davisfamily.warehouse.rendering.model.tracks.TrackSpec;
-import online.davisfamily.warehouse.rendering.model.tracks.WarehouseSegmentMetadata;
 import online.davisfamily.warehouse.rendering.model.tracks.WarehouseRouteBuilder;
 import online.davisfamily.warehouse.sim.tote.Tote;
 import online.davisfamily.warehouse.sim.totebag.control.ToteTrackTipperFlowController;
@@ -49,9 +41,6 @@ public class TipperEntryModule implements PackHandoffPointProvider {
     private static final float TIPPER_START_X = (TRACK_LENGTH - TIPPER_LENGTH) * 0.5f;
     private static final float TIPPER_STOP_DISTANCE = TIPPER_START_X + (TIPPER_LENGTH * 0.5f);
     private static final float TIPPER_TIPPED_ANGLE_RADIANS = -1.02f;
-    private static final float SLIDE_EXIT_WIDTH = 0.30f;
-    private static final float SLIDE_LENGTH = 1.20f;
-    private static final float SLIDE_THICKNESS = 0.03f;
     private static final float CONTAINED_PACK_GAP_X = 0.012f;
     private static final float CONTAINED_PACK_GAP_Z = 0.012f;
     private static final float CONTAINED_PACK_GAP_Y = 0.010f;
@@ -77,6 +66,7 @@ public class TipperEntryModule implements PackHandoffPointProvider {
     private final Map<String, Vec3> containedPackLayoutById;
     private final TipperToSorterDischargeSeam dischargeSeam;
     private final TipperEntryPackVisuals packVisuals;
+    private final TipperAssemblyFactory tipperAssemblyFactory;
 
     public TipperEntryModule(
             TriangleRenderer tr,
@@ -201,44 +191,27 @@ public class TipperEntryModule implements PackHandoffPointProvider {
         tipperTrackRuntimeState = new ConveyorRuntimeState();
         tipperTrackRuntimeState.setRunning(true);
         float tipperTrackOverallWidth = tipperTrackSpec.getOverallWidth();
-        slideEntryWidth = TIPPER_LENGTH;
         Vec3 tipperAssemblyLocalOrigin = new Vec3(
                 TIPPER_STOP_DISTANCE,
                 0.02f,
                 TRACK_Z - (tipperTrackOverallWidth * 0.5f));
-        tipperAssemblyRenderable = createAnchor("tipper_assembly");
         Vec3 tipperAssemblyWorld = localToWorld(tipperAssemblyLocalOrigin);
-        tipperAssemblyRenderable.transformation.xTranslation = tipperAssemblyWorld.x;
-        tipperAssemblyRenderable.transformation.yTranslation = tipperAssemblyWorld.y;
-        tipperAssemblyRenderable.transformation.zTranslation = tipperAssemblyWorld.z;
-        tipperAssemblyRenderable.transformation.angleY = rigYaw();
-
-        RenderableObject tipperTrackRenderable = createLocalTipperTrack(tr, tipperTrackSpec, trackAppearance, tipperTrackRuntimeState);
-        tipperTrackRenderable.transformation.zTranslation = tipperTrackOverallWidth * 0.5f;
-        RenderableObject tipperSlideRenderable = createFunnelSlideRenderable("tipper_slide", 0xFFFF00FF);
-        tipperSlideRenderable.transformation.xTranslation = 0f;
-        tipperSlideRenderable.transformation.yTranslation = 0.04f;
-        tipperSlideRenderable.transformation.zTranslation = -0.02f;
-        tipperSlideRenderable.addChild(createSlideGuide(
-                "tipper_slide_left_guide",
-                -(slideEntryWidth * 0.5f) + 0.015f,
-                slideEntryWidth,
-                SLIDE_EXIT_WIDTH));
-        tipperSlideRenderable.addChild(createSlideGuide(
-                "tipper_slide_right_guide",
-                (slideEntryWidth * 0.5f) - 0.015f,
-                slideEntryWidth,
-                SLIDE_EXIT_WIDTH));
-
-        tipperAssemblyRenderable.addChild(tipperTrackRenderable);
-        tipperAssemblyRenderable.addChild(tipperSlideRenderable);
+        tipperAssemblyFactory = new TipperAssemblyFactory();
+        TipperAssemblyFactory.BuildResult assemblyBuild = tipperAssemblyFactory.build(
+                tr,
+                tipperTrackSpec,
+                trackAppearance,
+                tipperTrackRuntimeState,
+                tipperAssemblyWorld,
+                rigYaw(),
+                TIPPER_LENGTH,
+                tipperTrackOverallWidth);
+        tipperAssemblyRenderable = assemblyBuild.assemblyRenderable();
+        slideEntryWidth = assemblyBuild.slideEntryWidth();
 
         Vec3 dischargeToteInteriorLocal = new Vec3(0f, toteInteriorFloorLocalY + 0.06f, 0.10f);
         Vec3 dischargeLidLocal = new Vec3(0f, 0.24f, -0.08f);
-        Vec3 dischargeSlideEntryLocal = new Vec3(
-                0f,
-                tipperSlideRenderable.transformation.yTranslation + 0.01f,
-                tipperSlideRenderable.transformation.zTranslation - 0.02f);
+        Vec3 dischargeSlideEntryLocal = assemblyBuild.dischargeSlideEntryLocal();
 
         objects.add(toteRenderable);
         objects.add(tipperAssemblyRenderable);
@@ -345,125 +318,6 @@ public class TipperEntryModule implements PackHandoffPointProvider {
                         new PackPlan("pack-b2", "bag-b", new PackDimensions(0.19f, 0.11f, 0.09f))));
     }
 
-    private RenderableObject createBox(
-            String id,
-            float length,
-            float height,
-            float width,
-            int colour) {
-        return createBox(id, length, height, width, colour, true);
-    }
-
-    private RenderableObject createBox(
-            String id,
-            float length,
-            float height,
-            float width,
-            int colour,
-            boolean selectable) {
-        return RenderableObject.create(
-                id,
-                tr,
-                RollerMeshFactory.createBoxRollerMesh(length, height, width),
-                new ObjectTransformation(0f, 0f, 0f, 0f, 0f, 0f, new Mat4()),
-                new OneColourStrategyImpl(colour),
-                selectable);
-    }
-
-    private RenderableObject createFunnelSlideRenderable(String id, int colour) {
-        return RenderableObject.create(
-                id,
-                tr,
-                createFunnelSlideMesh(),
-                new ObjectTransformation(0f, 0f, 0f, 0f, 0f, 0f, new Mat4()),
-                new OneColourStrategyImpl(colour),
-                true);
-    }
-
-    private RenderableObject createSlideGuide(String id, float xOffset, float entryWidth, float exitWidth) {
-        boolean left = xOffset < 0f;
-        float wallThickness = 0.02f;
-        float wallHeight = 0.07f;
-        float yBottom = SLIDE_THICKNESS * 0.5f;
-        float yTop = yBottom + wallHeight;
-        float zStart = 0f;
-        float zEnd = -SLIDE_LENGTH;
-        float outerEntryX = left ? -(entryWidth * 0.5f) : (entryWidth * 0.5f);
-        float outerExitX = left ? -(exitWidth * 0.5f) : (exitWidth * 0.5f);
-        float innerEntryX = left ? outerEntryX + wallThickness : outerEntryX - wallThickness;
-        float innerExitX = left ? outerExitX + wallThickness : outerExitX - wallThickness;
-        float startMinX = Math.min(innerEntryX, outerEntryX);
-        float startMaxX = Math.max(innerEntryX, outerEntryX);
-        float endMinX = Math.min(innerExitX, outerExitX);
-        float endMaxX = Math.max(innerExitX, outerExitX);
-
-        Vec4[] vertices = new Vec4[] {
-                new Vec4(startMinX, yBottom, zStart, 1f),
-                new Vec4(startMaxX, yBottom, zStart, 1f),
-                new Vec4(startMaxX, yTop, zStart, 1f),
-                new Vec4(startMinX, yTop, zStart, 1f),
-                new Vec4(endMinX, yBottom, zEnd, 1f),
-                new Vec4(endMaxX, yBottom, zEnd, 1f),
-                new Vec4(endMaxX, yTop, zEnd, 1f),
-                new Vec4(endMinX, yTop, zEnd, 1f)
-        };
-
-        int[][] triangles = new int[][] {
-                {0, 1, 2}, {0, 2, 3},
-                {4, 7, 6}, {4, 6, 5},
-                {0, 3, 7}, {0, 7, 4},
-                {1, 5, 6}, {1, 6, 2},
-                {0, 4, 5}, {0, 5, 1},
-                {3, 2, 6}, {3, 6, 7}
-        };
-        return RenderableObject.create(
-                id,
-                tr,
-                new Mesh(vertices, triangles, id + "_mesh"),
-                new ObjectTransformation(0f, 0f, 0f, 0f, 0f, 0f, new Mat4()),
-                new OneColourStrategyImpl(0xFF6E6A5A),
-                true);
-    }
-
-    private RenderableObject createLocalTipperTrack(
-            TriangleRenderer tr,
-            TrackSpec toteTrackSpec,
-            TrackAppearance trackAppearance,
-            ConveyorRuntimeState rollerRuntimeState) {
-        RouteSegment localTipperSegment = new RouteSegment(
-                "local_tipper_track",
-                new LinearSegment3(
-                        new Vec3(-TIPPER_LENGTH * 0.5f, 0f, 0f),
-                        new Vec3(TIPPER_LENGTH * 0.5f, 0f, 0f),
-                        false));
-        RenderableObject renderable = RenderableTrackFactory.createRenderableTrack(
-                tr,
-                localTipperSegment,
-                new WarehouseSegmentMetadata(),
-                toteTrackSpec,
-                trackAppearance,
-                rollerRuntimeState);
-        renderable.transformation.yTranslation = 0f;
-        return renderable;
-    }
-
-    private RenderableObject createAnchor(String id) {
-        return RenderableObject.create(
-                id,
-                tr,
-                new online.davisfamily.threedee.model.Mesh(
-                        new online.davisfamily.threedee.matrices.Vec4[] {
-                                new online.davisfamily.threedee.matrices.Vec4(0f, 0f, 0f, 1f),
-                                new online.davisfamily.threedee.matrices.Vec4(0f, 0f, 0f, 1f),
-                                new online.davisfamily.threedee.matrices.Vec4(0f, 0f, 0f, 1f)
-                        },
-                        new int[][] { {0, 1, 2} },
-                        "anchor"),
-                new ObjectTransformation(0f, 0f, 0f, 0f, 0f, 0f, new Mat4()),
-                triangleIndex -> 0,
-                false);
-    }
-
     private Vec3 localToWorld(float localX, float localY, float localZ) {
         return localToWorld(new Vec3(localX, localY, localZ));
     }
@@ -472,36 +326,6 @@ public class TipperEntryModule implements PackHandoffPointProvider {
         Vec3 rotated = Vec3.rotateY(localPoint, rigYaw());
         rotated.mutableAdd(layoutSpec.origin());
         return rotated;
-    }
-
-    private Mesh createFunnelSlideMesh() {
-        float halfEntryWidth = slideEntryWidth * 0.5f;
-        float halfExitWidth = SLIDE_EXIT_WIDTH * 0.5f;
-        float yTop = SLIDE_THICKNESS * 0.5f;
-        float yBottom = -SLIDE_THICKNESS * 0.5f;
-        float zStart = 0f;
-        float zEnd = -SLIDE_LENGTH;
-
-        Vec4[] vertices = new Vec4[] {
-                new Vec4(-halfEntryWidth, yBottom, zStart, 1f),
-                new Vec4(halfEntryWidth, yBottom, zStart, 1f),
-                new Vec4(halfEntryWidth, yTop, zStart, 1f),
-                new Vec4(-halfEntryWidth, yTop, zStart, 1f),
-                new Vec4(-halfExitWidth, yBottom, zEnd, 1f),
-                new Vec4(halfExitWidth, yBottom, zEnd, 1f),
-                new Vec4(halfExitWidth, yTop, zEnd, 1f),
-                new Vec4(-halfExitWidth, yTop, zEnd, 1f)
-        };
-
-        int[][] triangles = new int[][] {
-                {0, 1, 2}, {0, 2, 3},
-                {4, 7, 6}, {4, 6, 5},
-                {0, 3, 7}, {0, 7, 4},
-                {1, 5, 6}, {1, 6, 2},
-                {0, 4, 5}, {0, 5, 1},
-                {3, 2, 6}, {3, 6, 7}
-        };
-        return new Mesh(vertices, triangles, "tipper_funnel_slide");
     }
 
     private float rigYaw() {
